34418bc1762bfb44cdcf8422195f5d294adf5846
ChristianCurzi <91216278+ChristianCurzi@users.noreply.github.com>
2023-08-08 16:35:09 +0200

    Calendar inizio

--- app/src/main/java/com/programmazionemobile/formula1app/CalendarFragment.kt
+++ app/src/main/java/com/programmazionemobile/formula1app/CalendarFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.programmazionemobile.formula1app.adapter.CalendarAdapter
+import com.programmazionemobile.formula1app.adapter.DriversStandingsAdapter
 
 class CalendarFragment : Fragment() {
 
@@ -16,33 +20,25 @@ class CalendarFragment : Fragment() {
 //        }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        // Trova il RecyclerView nel layout del fragment
+        val rv: RecyclerView = view.findViewById(R.id.calendar_rv)
+
+        // Configura il LayoutManager per il RecyclerView (LinearLayoutManager in questo caso)
+        val layoutManager = LinearLayoutManager(requireContext())
+        rv.layoutManager = layoutManager
+
+        // Crea l'adapter e assegna al RecyclerView
+        val data = IntRange(0, 10).toList()
+        val adapter = CalendarAdapter(data)
+        rv.adapter = adapter
+
         return view
+
+//        return inflater.inflate(R.layout.fragment_drivers_standings, container, false)
     }
-
-
-   /* override fun onViewCreated(view: View, savedInstanceState: Bundle?){
-//        R.id.textViewCalendar
-    }*/
-
-/*    companion object {
-        *//**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFragment.
-         *//*
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                CalendarFragment().apply {
-//                    arguments = Bundle().apply {
-//
-//                    }
-                }
-    }*/
 }
