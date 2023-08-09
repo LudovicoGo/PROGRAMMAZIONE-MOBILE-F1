dae85dc0a011ab7d3d672499e1231103a5617471
Ludovico Gorgoglione <S1099130@studenti.univpm.it>
2023-08-09 12:15:18 +0200

    asdasd

--- app/src/main/java/com/programmazionemobile/formula1app/adapter/DriversStandingsAdapter.kt
+++ app/src/main/java/com/programmazionemobile/formula1app/adapter/DriversStandingsAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.programmazionemobile.formula1app.R
 
@@ -16,7 +18,15 @@ class DriversStandingsAdapter(val data: List<Int>) :  RecyclerView.Adapter<Drive
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DriversStandingsAdapter.DriversStandingsViewHolder {
         val layout = LayoutInflater.from(parent.context).inflate(R.layout.drivers_standings_recycler_view, parent, false)
-        return DriversStandingsViewHolder(layout)
+        val holder =  DriversStandingsViewHolder(layout)
+        holder.row.setOnClickListener{
+                view -> view.findNavController().navigate(R.id.action_driversStandingsFragment_to_driverProfileFragment)
+            /*
+            val navController = findNavController()
+            // Esegui la navigazione verso il FragmentB
+            navController.navigate(R.id.action_driversStandingsFragment_to_driverProfileFragment)*/
+        }
+        return holder
     }
 
     override fun onBindViewHolder(holder: DriversStandingsAdapter.DriversStandingsViewHolder, position: Int) {
