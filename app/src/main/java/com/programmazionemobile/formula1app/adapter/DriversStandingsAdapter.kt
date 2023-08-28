805d98f6de4ed97f9bee8f7153e629eed490a921
ChristianCurzi <91216278+ChristianCurzi@users.noreply.github.com>
2023-08-28 11:05:49 +0200

    cicasc

--- app/src/main/java/com/programmazionemobile/formula1app/adapter/DriversStandingsAdapter.kt
+++ app/src/main/java/com/programmazionemobile/formula1app/adapter/DriversStandingsAdapter.kt
@@ -1,22 +1,37 @@
 package com.programmazionemobile.formula1app.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.programmazionemobile.formula1app.R
 import com.programmazionemobile.formula1app.data.driverStandingsData.DriverStanding
+import kotlin.random.Random
 
 class DriversStandingsAdapter(val data: MutableList<DriverStanding>, val context: Context) :
-    RecyclerView.Adapter<DriversStandingsAdapter.DriversStandingsViewHolder>() {
+    RecyclerView.Adapter<ViewHolder>() {
+    //RecyclerView.Adapter<DriversStandingsAdapter.DriversStandingsViewHolder>() {
     //class DriversStandingsAdapter() :  RecyclerView.Adapter<DriversStandingsAdapter.DriversStandingsViewHolder>() {
+
+    private val TYPE_HEADER = 0
+    private val TYPE_ITEM = 1
+
+    class HeaderViewHolder(val row: View): RecyclerView.ViewHolder(row){
+
+    }
+
     class DriversStandingsViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
 
         val DriverName = row.findViewById<TextView>(R.id.DriverName)
@@ -27,87 +42,109 @@ class DriversStandingsAdapter(val data: MutableList<DriverStanding>, val context
         val DriverTeam = row.findViewById<TextView>(R.id.DriverTeam)
         val DriverPoints = row.findViewById<TextView>(R.id.DriverPoints)
         val DriverImage = row.findViewById<ImageView>(R.id.DriverImage)
-//        val DriverImage = row.findViewById<ImageView>(R.id.DriverImage)
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): DriversStandingsAdapter.DriversStandingsViewHolder {
-        val layout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.drivers_standings_recycler_view, parent, false)
-        val holder = DriversStandingsViewHolder(layout)
+    ): ViewHolder {
 
-        /*        holder.row.setOnClickListener { view ->
-                    val bundle = Bundle()
-                    bundle.putString("driverName", data[position].driverName)
-                    bundle.putString("driverPosition", data[position].driverPosition)
+        return when (viewType) {
+            TYPE_ITEM -> {
+                val itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.drivers_standings_recycler_view, parent, false)
+                DriversStandingsViewHolder(itemView)
+            }
 
-                    val action =
-                        DriversStandingsFragmentDirections.actionDriversStandingsFragmentToDriverProfileFragment()
-        //            view.findNavController().navigate(R.id.action_driversStandingsFragment_to_driverProfileFragment)
-                    view.findNavController().navigate(action)
-                }*/
+            TYPE_HEADER -> {
+                val headerView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.header_driver_standings, parent, false)
+                HeaderViewHolder(headerView)
+            }
 
-
-        return holder
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+        /*
+                val layout = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.drivers_standings_recycler_view, parent, false)
+                val holder = DriversStandingsViewHolder(layout)
+                return holder*/
     }
 
-    override fun onBindViewHolder(
-        holder: DriversStandingsAdapter.DriversStandingsViewHolder,
-        position: Int
-    ) {
-        holder.DriverPosition.text = data.get(position).positionText
-        holder.DriverID.text = data.get(position).driver.driverId
-        holder.DriverName.text = data[position].driver.givenName
-        holder.DriverFamilyName.text = data[position].driver.familyName
-        holder.DriverPoints.text = data[position].points
-        holder.DriverNumber.text = data[position].driver.permanentNumber
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
 
-        val driverId = data[position].driver.driverId // ID del pilota
-        val drawableResId =
-            context.resources.getIdentifier(driverId, "drawable", context.packageName)
-        if (drawableResId != 0) {
-            // Se l'ID è diverso da 0, il drawable è stato trovato
-            holder.DriverImage.setImageResource(drawableResId) // Imposta il drawable dell'ImageView
-        } else {
-            // Il drawable non è stato trovato
+        when(holder){
+
+            is HeaderViewHolder -> {
+
+            }
+
+            is DriversStandingsViewHolder -> {
+                if(position != TYPE_HEADER) {
+                    val itemPosition = position - 1
+
+                    holder.DriverPosition.text = data.get(itemPosition).positionText
+                    holder.DriverID.text = data.get(itemPosition).driver.driverId
+                    holder.DriverName.text = data[itemPosition].driver.givenName
+                    holder.DriverFamilyName.text = data[itemPosition].driver.familyName
+                    holder.DriverPoints.text = data[itemPosition].points
+                    holder.DriverNumber.text = data[itemPosition].driver.permanentNumber
+
+
+                    val driverId = data[itemPosition].driver.driverId // ID del pilota
+                    val drawableResId =
+                        context.resources.getIdentifier(driverId, "drawable", context.packageName)
+                    if (drawableResId != 0) {
+                        // Se l'ID è diverso da 0, il drawable è stato trovato
+                        holder.DriverImage.setImageResource(drawableResId) // Imposta il drawable dell'ImageView
+                    } else {
+                        // Il drawable non è stato trovato
+                    }
+                    var driverConstructor = ""
+
+
+                    for (constructor in data[itemPosition].constructors) {
+                        driverConstructor = driverConstructor + constructor.name + " "
+                    }
+                    holder.DriverTeam.text = driverConstructor
+
+                    holder.row.setOnClickListener { view ->
+                        val bundle = Bundle()
+                        bundle.putString("DriverID", data[itemPosition].driver.driverId)
+                        bundle.putString("DriverName", data[itemPosition].driver.givenName)
+                        bundle.putString("DriverFamilyName", data[itemPosition].driver.familyName)
+                        bundle.putString("DriverCountry", data[itemPosition].driver.nationality)
+                        bundle.putString("DriverNumber", data[itemPosition].driver.permanentNumber)
+                        bundle.putString("DriverBirth", data[itemPosition].driver.dateOfBirth)
+                        bundle.putString("DriverCurrentSeasonPosition", data[itemPosition].position)
+                        bundle.putString("DriverPoints", data[itemPosition].points)
+                        bundle.putString("DriverTeam", driverConstructor)
+
+                        view.findNavController().navigate(R.id.driverProfileFragment, bundle)
+                    }
+                }
+                else{
+//            val items = arrayOf("Item 1", "Item 2", "Item 3") // Elenca i tuoi elementi
+
+                    /// TODO: ddddddddddd
+                }
+            }
         }
-        var driverConstructor = ""
-
-
-        for (constructor in data[position].constructors) {
-            driverConstructor = driverConstructor + constructor.name + " "
-        }
-        holder.DriverTeam.text = driverConstructor
-
-
-
-
-        holder.row.setOnClickListener { view ->
-            val bundle = Bundle()
-            bundle.putString("DriverID", data[position].driver.driverId)
-            bundle.putString("DriverName", data[position].driver.givenName)
-            bundle.putString("DriverFamilyName", data[position].driver.familyName)
-            bundle.putString("DriverCountry", data[position].driver.nationality)
-            bundle.putString("DriverNumber", data[position].driver.permanentNumber)
-            bundle.putString("DriverBirth", data[position].driver.dateOfBirth)
-            bundle.putString("DriverCurrentSeasonPosition", data[position].position)
-            bundle.putString("DriverPoints", data[position].points)
-            bundle.putString("DriverTeam", driverConstructor)
-
-            view.findNavController().navigate(R.id.driverProfileFragment, bundle)
-        }
-
-
-
-
-
     }
+
+
 
     override fun getItemCount(): Int {
-        return data.size
+        return data.size + 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            TYPE_HEADER
+        } else {
+            TYPE_ITEM
+        }
     }
 
     fun showDriverStandings(newDriverStandings: MutableList<DriverStanding>) {
